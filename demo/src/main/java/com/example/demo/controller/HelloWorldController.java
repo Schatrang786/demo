0698ae02ffa69315306c31678458133dc36567aa
zeesh <zeesh@192.168.0.12>
2024-02-19 22:49:29 +0100

    First commit

---
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping("/hallo")
+	public List<Integer> hello() {
+		return getNumber();
+	}
+	
+	private List<Integer> getNumber(){
+		Integer[] week = {1,2343,34,2,44,8,9,0};
+		Arrays.sort(week);
+		System.out.println("das ist auf der System.out.println");
+		return Arrays.asList(week);
+	}
+	
+	
+	private String messageString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Personalnummer;");
+		sb.append("Sozialversicherungsnummer;");
+		sb.append("Email");
+		return sb.toString();
+		
+	}
+	@GetMapping("/test")
+	public String message() {
+		return messageString();
+	}
+}
