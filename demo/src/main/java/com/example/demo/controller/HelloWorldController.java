8426cbe380bf6eb841ed964acb627455c63c39b8
zeesh <zeesh@192.168.0.12>
2024-02-20 21:13:43 +0100

    some changes how to display on the browser

--- demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloWorldController {
@@ -18,13 +23,20 @@ public class HelloWorldController {
 		Integer[] week = {1,2343,34,2,44,8,9,0};
 		Arrays.sort(week);
 		System.out.println("Github commit");
-		return Arrays.asList(week);
+		List<Integer> resultList = new ArrayList<>();
+		Integer valueInteger;
+		for (Integer integer : week) {
+			valueInteger = integer;
+			resultList.add(valueInteger);
+		}
+		return resultList;
 	}
 	
 	
 	private Map<String, Integer> getMap(){
 		Map<String, Integer> myMap = new HashMap<String, Integer>();
-		myMap.put("foo", 3);
+		myMap.put("Deutschland", +49);
+		myMap.put("Pakistan", +92);
 		return myMap;
 	}
 	
@@ -44,6 +56,29 @@ public class HelloWorldController {
 	//test
 	@GetMapping("/test")
 	public String message() {
-		return messageString();
+		 String[] week = {"Monday", "Tuseday", "Wednesday", "Thursday"};
+	       
+		 StringBuilder result = new StringBuilder();
+		 //String value = null;
+	        for (String weekdays : week) {
+	        	result.append(weekdays).append("<br>");
+	        	//System.out.println(weekdays);
+	        }
+
+	        return result.toString().trim();
 	}
+	@GetMapping("/void")
+	public void getMessage() {
+		StringBuilder result = new StringBuilder();
+		result.append("Hallo Hamsa!");
+		result.toString();
+		message(); 
+		//System.out.println("Hallo Hamsa!");
+	}
+	 @GetMapping("/displayVoid")
+	    public void displayMessage(HttpServletResponse response) throws IOException {
+	        String message = "Hello, Hamsa!";
+	        response.getWriter().write(message);
+	        response.getWriter().write(message());
+	    }
 }
